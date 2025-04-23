df908aa951c2f93a8dd282d3d2920b45f82a2642
MRezaOsfoori <mrezaosfoori@aut.ac.ir>
2025-04-23 17:11:09 +0330

    start dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import icons from "../lib/icons";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="p-4 flex flex-col gap-6  ">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-4">
+          <p>بیا شروع کنیم!</p>
+          <StepTitle title={"ایجاد برنامه"} isDone={true} />
+          <StepTitle title={"دعوت مشتری"} isDone={false} />
+          <StepTitle title={"برنامه رو منتقل کن"} isDone={false} />
+        </div>
+        <div className="bg-slate-500 py-2 px-4 h-full">
+          <img src={icons.search} alt="" className="w-20 h-20" />
+          <p>راهنمای استفاده ار وبسایت</p>
+        </div>
+      </div>
 
-export default Dashboard
+      <div className="flex flex-col md:flex-row justify-between w-full gap-2">
+        <div className="flex flex-col gap-1">
+          <p className="text-[20px] font-bold">سلام محمدرضا !</p>
+          <p className="text-[16px] font-semibold text-slate-800 ">
+            یک آمار بگیر از اوضاع مشتریات
+          </p>
+        </div>
+        <div className="flex gap-4  max-h-[60px]">
+          <div className="relative">
+            <img
+              src={icons.search}
+              alt=""
+              className="w-4 h-4 absolute top-1/2 -translate-y-1/2 left-3"
+            />
+            <input
+              type="text"
+              placeholder="..."
+              className="bg-slate-100 rounded-md md:min-w-[200px] h-full px-8 py-2"
+            />
+          </div>
+          <div className="bg-red-900 text-white px-1 md:px-4 rounded-lg py-1 md:py-2 flex gap-2 items-center cursor-pointer">
+            <img src={icons.plus} alt="" className="w-4 h-4 invert" />
+            <button>افزودن مشتری</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex   gap-4">
+        <ActivityCard title={"ایجاد برنامه"} value={1} />
+        <ActivityCard title={"دعوت مشتری"} value={3} />
+        <ActivityCard title={"برنامه رو منتقل کن"} value={6} />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
+
+const StepTitle = ({ title, isDone }) => {
+  return (
+    <div className="flex gap-2">
+      <img src={icons.search} alt="" className="w-6 h-6" />
+      <p>{title}</p>
+    </div>
+  );
+};
+
+const ActivityCard = ({ title, activity }) => {
+  return (
+    <div className="flex flex-col gap-2 bg-white">
+      <div className="flex justify-between">
+        <p>{title}</p>
+        <img src={icons.search} alt="" className="w-6 h-6" />
+      </div>
+      <div className="flex justify-between">
+        <p>{activity}</p>
+        <img src={icons.avatar} alt="" className="w-6 h-6" />
+      </div>
+    </div>
+  );
+};
